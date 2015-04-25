139b3cef859052de794e83fad9ece43bd94d9994
Ben Stokoe <benstokoe1@googlemail.com>
2015-04-25 12:18:11 +0100

    Add todo save on keydown

--- js/components/TodoTextInput.react.js
+++ js/components/TodoTextInput.react.js
@@ -5,18 +5,26 @@ class TodoTextInput extends React.Component {
     constructor(props) {
         super(props)
         this._save = this._save.bind(this);
+        this._onKeyDown = this._onKeyDown.bind(this);
     }
 
     render() {
         return (
             <input
                 onBlur={ this._save } 
+                onKeyDown={ this._onKeyDown }
             />
         );
     }
 
     _save(event) {
         this.props.onSave(event.target.value);
+    }
+
+    _onKeyDown(event) {
+        if (event.keyCode === 13) {// enter
+            this._save(event); 
+        }
     }
 };
 
